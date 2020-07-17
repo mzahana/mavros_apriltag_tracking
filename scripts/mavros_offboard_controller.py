b4bbf2331b3bd65e57ced6b38b8ca7066c8fbf38
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2020-07-17 19:44:23 +0400

    Update license

--- scripts/mavros_offboard_controller.py
+++ scripts/mavros_offboard_controller.py
@@ -9,8 +9,7 @@
  *   * Redistributions in binary form must reproduce the above copyright
  *     notice, this list of conditions and the following disclaimer in the
  *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived from
+ *   * NThe name(s) of the author(s) (Mohamed Abdelkader) may not be used to endorse or promote products derived from
  *     this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
